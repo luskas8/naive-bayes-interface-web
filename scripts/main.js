3ae65175a705c584c97650f349089caca47c2ce0
Luskas8 <gnomo1049@gmail.com>
2021-11-29 17:21:12 -0300

    API sendo consumida

--- scripts/main.js
+++ scripts/main.js
@@ -3,9 +3,18 @@ const names = document.querySelector("#names");
 const resposta = document.querySelector("#respostas");
 
 const BASE_URL = "https://interdisciplinar-prob-poo.herokuapp.com/naive_bayes";
+// const BASE_URL = "http://127.0.0.1:5000/naive_bayes";
 
 const handleResponse = (response) => {
-  console.log(response);
+  const data = response.names_by_gender;
+  let text_resposta = "";
+
+  Object.keys(data).map(key => {
+    text_resposta += `${key}: ${data[key]}\n`;
+  })
+
+  console.log(text_resposta);
+  resposta.value = text_resposta;
 }
 
 /**
@@ -25,23 +34,25 @@ const submit = (e) => {
 
   let namesData = names.value;
 
+  if (namesData === "") {
+    resposta.value = "Nada para descobrir!";
+    return;
+  }
+
   resposta.value = "Descobrindo...";
-  resposta.disabled = true;
 
   if (typeof namesData !== 'object') {
     namesData = namesData.split(",").map(name => { return name.replace(" ", "").replace("\n", "") });
   }
 
   const data = { "nomes": namesData };
-  console.log(data);
 
   fetch(BASE_URL, {
     headers: {
-      "Access-Control-Allow-Origin": "*",
-      'Access-Control-Allow-Headers': "*",
+      "Content-Type": "application/json",
     },
     method: 'POST',
-    body: JSON.parse({"nomes": namesData}),
+    body: JSON.stringify(data),
     mode: 'cors',
   })
     .then(rawData => { return rawData.json() })
@@ -51,6 +62,7 @@ const submit = (e) => {
 
 const load = () => {
   form.addEventListener('submit', submit);
+  resposta.disabled = true;
 }
 
 window.addEventListener('load', load)
